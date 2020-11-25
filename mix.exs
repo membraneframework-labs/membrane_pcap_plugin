9b406967495af8ef6a2bd957664d959214288b7b
Mateusz Front <mateusz.front@swmansion.com>
2020-11-25 14:25:38 +0100

    bump to 0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Element.Pcap.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @github_url "https://github.com/membraneframework/membrane-element-pcap"
 
   def project do
@@ -64,7 +64,7 @@ defmodule Membrane.Element.Pcap.MixProject do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.5.0"},
+      {:membrane_core, "~> 0.6.0"},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
       {:expcap, github: "Hajto/expcap"},
