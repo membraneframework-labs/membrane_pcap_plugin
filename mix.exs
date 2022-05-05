67a1a8bf2f825805d7f67198f85ec01876f2ea64
varsill <lukasz.kita0@gmail.com>
2022-05-05 09:19:06 +0200

    bump into 0.6.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Pcap.MixProject do
   use Mix.Project
 
-  @version "0.6.0"
+  @version "0.6.1"
   @github_url "https://github.com/membraneframework/membrane_pcap_plugin"
 
   def project do
